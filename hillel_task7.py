fbe6cc7e7f390662131df06864defae8366bcf25
liliia.khomyn <liliia.khomyn@pm.bet>
2021-12-05 21:35:13 +0200

    homewok lesson 4

---
+++ hillel_task7.py
@@ -0,0 +1,10 @@
+string = 'aaa bbb ccc ddd bbb rrr eee ddd bbb'
+new_data = 'bbb'
+count=0
+
+while string.find(new_data) != -1:
+    string = string.replace(new_data,'xxx', 1)
+    count = count + 1
+
+print (string.replace('bbb','xxx'))
+print count
